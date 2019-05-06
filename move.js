385591b87c4aa58926ec20d540d734477ff9f100
Taras Kolomatski <taras.kolomatski@uwaterloo.ca>
2019-05-05 22:41:06 -0400

    Rectangles move around

--- move.js
+++ move.js
@@ -1,15 +1,13 @@
 ;(function ($, window, document) {
   var startHandler = function(e, point){
-    e.preventDefault();
-
     var active_pt = this;
 
     $(this).addClass('active');
 
     var rect = active_pt.getBoundingClientRect();
 
-    point.x = e.pageX - rect.left + 3; // 3 is the margin size
-    point.y = e.pageY - rect.top + 3;
+    point.active_x = e.pageX - rect.left + 1; // 3 is the margin size
+    point.active_y = e.pageY - rect.bottom - 1;
 
     $(document).on("mousemove", function (e){
       moveHandler.call(active_pt, e, point);
@@ -19,52 +17,66 @@
     });
   },
   moveHandler = function(e, point){
-    e.preventDefault();
-
     var square = point.parent.get(0).getBoundingClientRect();
 
-    var x = e.pageX - square.left - point.x,
-    y = e.pageY - square.top - point.y;
+    var x = e.pageX - square.left - point.active_x,
+    y = square.bottom - e.pageY + point.active_y;
 
     this.style.left = x + 'px';
-    this.style.top = y + 'px';
+    this.style.bottom = y + 'px';
+
+    point.area.style.width = (x - point.origin_x) + 'px';
+    point.area.style.height = (y - point.origin_y) + 'px';
   },
 
   endHandler = function(e, point){
-    e.preventDefault();
     $(this).removeClass('active');
     $(document).off("mousemove");
-    point.x = null;
-    point.y = null;
+    point.active_x = null;
+    point.active_y = null;
   };
 
-  function addPointToDOM(point){
-    var elt = $('<div id="' + point.name + '" class="point"> </div>').css({
-      'top' :'0px',
-      'left' : '0px'
+  function addPointToDOM(point, x, y){
+    var tip = $('<div id="' + point.name + '" class="point"> </div>').css({
+      'left' : x - 7 + 'px',
+      'bottom' : y - 7 + 'px'
     });
 
-    elt.on("mousedown", function (e){
-      /*point.x += 20;
-      point.y += 20;
-      elt.css({
-        'top' : point.x +'px',
-        'left' : point.y + 'px'
-      });*/
+    var area = $('<div id="rect_' + point.name + '" class="rect"> </div>').css({
+      'height' : 0 + 'px',
+      'width' : 0 + 'px',
+      'left' : x - 1 + 'px',
+      'bottom' : y - 1 + 'px'
+    });
+
+    tip.on("mousedown", function (e){
+      e.stopPropagation();
       startHandler.call(this, e, point);
     });
 
-    point.parent.append(elt);
+    area.on("mousedown", function (e){
+      e.stopPropagation();
+    });
+
+    point.parent.append(tip);
+    point.parent.append(area);
+
+    point.area = $('#rect_' + point.name).get(0);
   }
 
-  function Point(name, parent_id){
-    this.parent = $(parent_id);
+  function Point(name, parent_id, x, y){
+    this.parent = $('#' + parent_id);
     this.name = name;
+
+    this.area = null;
+
+    this.origin_x = x - 7;
+    this.origin_y = y - 7;
 
     //These is where an active element was originally clicked
     this.active_x = null;
     this.active_y = null;
-    addPointToDOM(this);
+    addPointToDOM(this, x, y);
   }
 
   Point.prototype = {
@@ -75,7 +87,19 @@
 } (jQuery, window, document));
 
 ;(function(){
+  var counter = 1;
+
+  var addHandler = function(e){
+    var square = this.getBoundingClientRect();
+    new Point('pt' + counter, this.id, e.pageX - square.left, square.bottom - e.pageY);
+    counter++;
+  }
+
   $(document).ready(function() {
-    var test = new Point('pt', '#square');
+    new Point('pt0', 'square', 0, 0);
+    $('#square').on('mousedown', function (e){
+      e.stopPropagation();
+      addHandler.call(this, e);
+    });
   });
 }());
